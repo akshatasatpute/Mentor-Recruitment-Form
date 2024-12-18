58fffa2943ac0cb0511a4173b70244817787ae8e
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-12-18 14:54:08 +0530

    Update Mentor_Rec.py

--- Scripts/Mentor_Rec.py
+++ Scripts/Mentor_Rec.py
@@ -55,7 +55,7 @@ st.markdown(
 )
 
 st.markdown(
-    "<div style='text-align: center;margin-bottom: 25px;'>"
+    "<div style='text-align: center;margin-bottom: 30px;'>"
     "<span style='font-size: 34px; font-weight: bold;'>Join 'She for STEM' Movement</span><br>"
     "<span style='font-size: 22px;font-weight: bold;'>Sign up to shape the future of next generation in STEM</span>"
     "</div>", 
