c72604c9c24fb4629ce453da0f8ca10c2c6c879c
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-12-18 14:34:49 +0530

    Update Mentor_Rec.py

--- Scripts/Mentor_Rec.py
+++ Scripts/Mentor_Rec.py
@@ -55,14 +55,19 @@ st.markdown(
 )
 
 st.markdown(
-    "<h1 style='color: black; font-weight: bold; text-align: center;margin-bottom: 5px;'> Join 'She for STEM' Movement </h1>", 
+    "<div style='text-align: center;'>"
+    "<h1 style='color: black; font-weight: bold; text-align: center;margin-bottom: 5px;'> Join 'She for STEM' Movement </h1>"
+    "<h4 style='color: black;font-weight: bold;font-size: 16px; text-align: center;margin-top: 0; margin-bottom: 5px;'>Sign up to shape the future of next generation in STEM</h4>"
+    "</div>", 
     unsafe_allow_html=True
+    #"<h1 style='color: black; font-weight: bold; text-align: center;margin-bottom: 5px;'> Join 'She for STEM' Movement </h1>", 
+    #unsafe_allow_html=True
 )
 
-st.markdown(
-    "<h2 style='color: black;font-weight: bold;font-size: 16px; text-align: center;margin-top: 0; margin-bottom: 5px;'>Sign up to shape the future of next generation in STEM</h2>", 
-    unsafe_allow_html=True
-)
+#st.markdown(
+    #"<h2 style='color: black;font-weight: bold;font-size: 16px; text-align: center;margin-top: 0; margin-bottom: 5px;'>Sign up to shape the future of next generation in STEM</h2>", 
+    #unsafe_allow_html=True
+#)
 
 Name=st.text_input("Enter your full name*")
 Email_id=st.text_input("Enter your email address (Work/Personal)*")
